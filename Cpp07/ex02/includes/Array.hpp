0a8a5fd3e1aa72ef80a01d5b7645f482144860cc
DigitalArtistry <code.42ai@protonmail.com>
2022-04-18 13:17:33 +0200

    ecole

--- Cpp07/ex02/includes/Array.hpp
+++ Cpp07/ex02/includes/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: lucisanc <lucisanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/17 19:59:59 by lucisanc          #+#    #+#             */
-/*   Updated: 2022/04/18 11:50:30 by lucisanc         ###   ########.fr       */
+/*   Updated: 2022/04/18 13:17:17 by lucisanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,27 @@ template<typename T>
 class	Array {
 
 public:
-	Array(void) : _nb(42) {return;};
-	Array( unsigned int nb) : _nb(nb) {return;};
-	~Array() {};
+	Array(void) : _arrP(NULL), _size(5) {return;};
+	Array( unsigned int size) : _arrP(new T[size], _size(size) {
+		for (unsigned int i = 0; i < size; i++) {
+			_arrP[i] = static_cast<T>(0);
+		}
+		return;
+	};
+	~Array() {delete [] _arrP;};
 	
-	unsigned int	getNb(void) {return _nb;}
+	void	copyArray( Array const &src, unsigned int size) {
+		for (unsigned int i = 0; i < size; i++) {
+			this->_arrP[i] = src._arrP[i];
+		}
+	};
+
+	
+	unsigned int	getSize(void) const {return _size;};
 
 private:
-	unsigned int	_nb;
+	T *				_arrP;
+	unsigned int	_size;
 };
 
 std::ostream &	operator<<(std::ostream &o, Array<unsigned int> &x) {
